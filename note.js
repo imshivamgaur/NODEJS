99d23aa2d8b149cee9e33c473fadc3cb49e6a6da
iamshivamgaur <shivam24august@gmail.com>
2025-07-16 13:28:51 +0530

    day 3 nodejs

--- note.js
+++ note.js
@@ -1,3 +1,5 @@
+//* =========================== MODULES IN NODE.JS ==========================
+
 // user-defined modules ==> module which are defined by the user
 
 // in order to export a module, we have two ways
@@ -5,8 +7,23 @@
 
 //! 2) module.exports = {variableName1, variableName2}; (using this we can export multiple variables at a time)
 
-//* for importing we have two ways
+//& for importing we have two ways
 //? the syntax is ==> let variableName = require("path of the file");
 
 //! let variableName = require("path");
-//! let {vairalbeName1, variableName2, ...} = require("path"); F
+//! let {variableName1, variableName2, ...} = require("path");
+
+
+//& Build-in modules ==> these modules are bundled with Node.js installation, also called as core modules
+
+// examples ==> fs, http, path, os, events, crypto
+
+//? syntax for importing build-in modules ==>
+// Let variableName = require("moduleName");
+
+
+
+//& ========================== FILE SYSTEM (FS) MODULE ==========================
+
+//! using js, we can interact with files and folders.
+//! Interaction (CRUD)
